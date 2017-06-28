59cfd09a6e72881911e92864516c939de7361f8e
jge <jge@expedia.com>
2017-06-27 23:51:26 -0500

    leetcode 95

--- UniqueBinarySearchTreeTwo.java
+++ UniqueBinarySearchTreeTwo.java
@@ -9,10 +9,66 @@
 //      3     2     1      1   3      2
 //     /     /       \                 \
 //    2     1         2                 3
-public class UniqueBinarySearchTreeTwo{
-  public List<TreeNode> generateTrees(int n){
-      
-  }
+
+import java.util.*;
+
+public class UniqueBinarySearchTreeTwo {
+    public List<TreeNode> generateTrees(int n){
+        if (n == 0){
+            return new ArrayList<>();
+        }
+        //recursion
+        return buildTrees(1, n);
+    }
+
+    private List<TreeNode> buildTrees(int lo, int hi){
+        List<TreeNode> result = new ArrayList<>();
+
+        // base
+        if(lo > hi){
+          return result;
+        }
+
+        if (lo == hi){
+            TreeNode node = new TreeNode(lo);
+            result.add(node);
+            return result;
+        }
+
+        //recursive function: divide & conquer
+        for (int i = lo; i <= hi; i++){
+            List<TreeNode> leftTrees = buildTrees(lo, i-1);
+            List<TreeNode> rightTrees = buildTrees(i+1, hi);
+
+            //merge
+            if(leftTrees.isEmpty() && rightTrees.isEmpty()){
+              TreeNode root = new TreeNode(i);
+              result.add(root);
+            }else if(leftTrees.isEmpty()){
+              for(TreeNode rightTree : rightTrees){
+                TreeNode root = new TreeNode(i);
+                root.right = rightTree;
+                result.add(root);
+              }
+            }else if(rightTrees.isEmpty()){
+              for(TreeNode leftTree : leftTrees){
+                TreeNode root = new TreeNode(i);
+                root.left = leftTree;
+                result.add(root);
+              }
+            }else{
+              for(TreeNode left : leftTrees){
+                for(TreeNode right : rightTrees){
+                  TreeNode root = new TreeNode(i);
+                  root.left = left;
+                  root.right = right;
+                  result.add(root);
+                }
+              }
+            }
+        }
+        return result;
+    }
 }
 
 class TreeNode {
